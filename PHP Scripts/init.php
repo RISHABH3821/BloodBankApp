c7dd8b1efce9f60c6c34994126f0b95fcae10ded
Rishabh Shrivastava <rishabhshri3821@gmail.com>
2020-01-04 11:34:49 +0530

    Update init.php

--- PHP Scripts/init.php
+++ PHP Scripts/init.php
@@ -2,7 +2,7 @@
     $host = "localhost"; //on same server we keep localhost
     $user = "YOUR_USER_ID_HERE";  //username of the database
     $pass = "YOUR_PASSWORD_HERE";   //password of the database
-    $db = "id10533884_blood_bank";  //name of database
+    $db = "YOUR_DB_NAME_HERE";  //name of database
     
     $con = mysqli_connect($host,$user,$pass,$db);
     
